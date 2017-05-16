218fe0a6a9d69e5a1e9607f0d0a1984fc69bf1c9
Maximilian Kögl <maximilian.koegl@stud.uni-regensburg.de>
2017-05-16 14:54:39 +0200

    Leere Hexagon Landschaft

---
+++ Assets/Scripts/Create_Hexagon_Map.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+
+    public GameObject hexPrefab;
+
+    //Amount of hex tiles
+    int width = 80;
+    int height = 80;
+
+    float xOffset = 1.8f;
+    float zOffset = 1.55f;
+	// Use this for initialization
+	void Start () {
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                float xPos = x*xOffset;
+                if( y % 2 == 1)
+                {
+                    xPos += xOffset/2;
+
+                }
+
+               GameObject hex_go = (GameObject)Instantiate(hexPrefab, new Vector3(xPos, 0, y*zOffset), Quaternion.identity);
+
+                hex_go.name = "Hex_" + x + "_" + y;
+
+                hex_go.transform.SetParent(this.transform);
+
+            }
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/MouseManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour {
+
+    public GameObject forrest;
+    public GameObject house;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            GameObject ourHitObject = hitInfo.collider.transform.gameObject;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                MeshRenderer mr = ourHitObject.GetComponentInChildren<MeshRenderer>();
+                GameObject parentHitObject = ourHitObject.transform.parent.gameObject;
+
+                GameObject forrest_go = (GameObject)Instantiate(forrest, parentHitObject.transform.position, Quaternion.identity);
+
+                Debug.Log(ourHitObject.transform.parent.gameObject.name);
+                Debug.Log(ourHitObject.transform.position);
+                Debug.Log(forrest_go.transform.position);
+
+                forrest_go.name = "Forrest_"+hitInfo.point;
+
+                forrest_go.transform.parent = parentHitObject.transform;
+
+                mr.material.color = Color.red;
+
+            }
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                Debug.Log(Input.GetMouseButtonDown(1));
+                MeshRenderer mr1 = ourHitObject.GetComponentInChildren<MeshRenderer>();
+                GameObject parentHitObject = ourHitObject.transform.parent.gameObject;
+
+                GameObject house_go = (GameObject)Instantiate(house, parentHitObject.transform.position, Quaternion.identity);
+
+                house_go.name = "House_" + hitInfo.point;
+
+                house_go.transform.parent = parentHitObject.transform;
+
+                mr1.material.color = Color.blue;
+
+            }
+
+
+        }
+
+	}
+}
